39475ef2b1ddaf3483660e307b3bfceb4dac1efc
gjjsgjj <a670155704g@qq.com>
2021-06-24 18:07:56 +0800

    增加hello接口测试test.http

--- src/main/java/com/unbeaten/wiki/controller/TestController.java
+++ src/main/java/com/unbeaten/wiki/controller/TestController.java
@@ -1,12 +1,21 @@
 package com.unbeaten.wiki.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 public class TestController {
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String hello() {
         return "Hello World!";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return "Hello World! Post, "+name;
     }
 }
