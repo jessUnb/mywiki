f2fc580c59f081832b674ffac8e9d94aa53e95ea
gjjsgjj <a670155704g@qq.com>
2021-06-24 16:24:03 +0800

    init

---
+++ src/main/java/com/unbeaten/wiki/MywikiApplication.java
@@ -0,0 +1,13 @@
+package com.unbeaten.wiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MywikiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MywikiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/unbeaten/wiki/MywikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unbeaten.wiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MywikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
