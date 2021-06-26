cb661a8aa1c677799d590c5ea65a372c676fbce8
gjjsgjj <a670155704g@qq.com>
2021-06-26 20:26:07 +0800

    5-4 集成图标组件 yarn add @ant-design/icons-vue

--- web/src/main.ts
+++ web/src/main.ts
@@ -2,9 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
 import Antd from "ant-design-vue";
 import "ant-design-vue/dist/antd.css";
+import * as Icons from '@ant-design/icons-vue';
 
 
 
@@ -15,3 +15,7 @@ app.use(Antd)
 
 app.mount('#app')
 
+const icons:any=Icons
+for (const i in icons) {
+    app.component(i,icons[i])
+}
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -48,9 +48,39 @@
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
-      <pre>
-{{ebooks}}
-      </pre>
+      <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
+        <template #footer>
+          <div>
+            <b>ant design vue</b>
+            footer part
+          </div>
+        </template>
+        <template #renderItem="{ item }">
+          <a-list-item key="item.title">
+            <template #actions>
+          <span v-for="{ type, text } in actions" :key="type">
+            <component v-bind:is="type" style="margin-right: 8px" />
+            {{ text }}
+          </span>
+            </template>
+            <template #extra>
+              <img
+                  width="272"
+                  alt="logo"
+                  src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+              />
+            </template>
+            <a-list-item-meta :description="item.description">
+              <template #title>
+                <a :href="item.href">{{ item.title }}</a>
+              </template>
+              <template #avatar><a-avatar :src="item.avatar" /></template>
+            </a-list-item-meta>
+            {{ item.content }}
+          </a-list-item>
+        </template>
+      </a-list>
+
 
     </a-layout-content>
   </a-layout>
@@ -59,6 +89,20 @@
 <script lang="ts">
 import axios from 'axios';
 import {defineComponent, onMounted, ref} from 'vue';
+
+const listData: any = [];
+
+for (let i = 0; i < 23; i++) {
+  listData.push({
+    href: 'https://www.antdv.com/',
+    title: `ant design vue part ${i}`,
+    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+    description:
+        'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+    content:
+        'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+  });
+}
 
 export default defineComponent({
   name: 'Home',
@@ -77,7 +121,19 @@ export default defineComponent({
 
 
     return{
-      ebooks
+      ebooks,
+      listData,
+      pagination : {
+        onChange: (page: number) => {
+          console.log(page);
+        },
+        pageSize: 3,
+      },
+      actions:  [
+      { type: 'StarOutlined', text: '156' },
+      { type: 'LikeOutlined', text: '156' },
+      { type: 'MessageOutlined', text: '2' },
+    ]
     }
 
 
