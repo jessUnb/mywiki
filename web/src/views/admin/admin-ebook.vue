2118d04b1b1be7a591aa44a90b14420f8401bb08
gjjsgjj <a670155704g@qq.com>
2021-06-28 19:35:03 +0800

    6-12 编辑时复制对象，修改表单时，不会影响列表数据

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -86,6 +86,7 @@
 import {defineComponent, onMounted, ref} from 'vue';
 import axios from 'axios';
 import {message} from "ant-design-vue";
+import {Tool} from "@/util/tool";
 
 export default defineComponent({
   name: 'AdminEbook',
@@ -202,7 +203,7 @@ export default defineComponent({
      */
     const edit = (record: any) => {
       modalVisible.value = true;
-      ebook.value = record
+      ebook.value = Tool.copy(record)
     };
 
     /**
