4005adf26d9faef5aa1cab733eaff6f1ec77f541
gjjsgjj <a670155704g@qq.com>
2021-06-28 17:40:06 +0800

    6-11 为保存接口增加参数校验

--- src/main/java/com/unbeaten/wiki/req/EbookSaveReq.java
+++ src/main/java/com/unbeaten/wiki/req/EbookSaveReq.java
@@ -3,6 +3,7 @@ package com.unbeaten.wiki.req;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 /**
@@ -27,6 +28,7 @@ public class EbookSaveReq implements Serializable {
     /**
      * 名称
      */
+    @NotNull(message = "【名称】不能为空")
     private String name;
 
     /**
--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -164,16 +164,17 @@ export default defineComponent({
     const handleModalOk = () => {
       modalLoading.value = true;
       axios.post("/ebook/save",ebook.value).then((response) => {
+        modalLoading.value = false;
         const data = response.data;
         if (data.success) {
           modalVisible.value = false;
-          modalLoading.value = false;
-
           //重新加载列表
           handleQuery({
             page: pagination.value.current,
             size: pagination.value.pageSize,
-          })
+          });
+        } else {
+          message.error(data.message)
         }
       });
     };
