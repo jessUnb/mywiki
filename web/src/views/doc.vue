dcebcf689c6c9a0fd8992793ea5d941ad05e5121
gjjsgjj <a670155704g@qq.com>
2021-06-30 16:26:00 +0800

    8-12 创建 doc.vue 文件,点击电子书，跳进文档显示页面

---
+++ web/src/views/doc.vue
@@ -0,0 +1,51 @@
+<template>
+  <a-layout>
+    <a-layout-content style="background: #fff;padding: 24px;margin: 0;min-height: 200px">
+      <a-row>
+        <a-col :span="6">
+          <a-tree
+              v-if="level1.length>0"
+              :tree-data="level1"
+              :replace-fields="{title: 'name',key: 'id',children: 'children'}"
+              :defaultExpandAll="true"
+          >
+          </a-tree>
+        </a-col>
+        <a-col :span="18">
+
+        </a-col>
+      </a-row>
+    </a-layout-content>
+  </a-layout>
+</template>
+
+<script>
+import {defineComponent, onMounted, ref} from "vue";
+import {message} from "ant-design-vue";
+import axios from "axios";
+import {Tool} from "@/util/tool";
+export default defineComponent({
+  name: 'doc',
+  setup(){
+    const level1 = ref();
+    level1.value = [];
+    const handleQuery = () => {
+      axios.get("/doc/all").then((response) => {
+        const data = response.data;
+        if(data.success){
+          level1.value = Tool.array2Tree(data.content,0);
+          console.log(level1.value);
+        }else{
+          message.error(data.message);
+        }
+      })
+    }
+    onMounted(()=>{
+      handleQuery();
+    })
+    return{
+      level1,
+    }
+  }
+})
+</script>
