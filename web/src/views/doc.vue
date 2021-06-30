6ce54554f27ae72c250f586b48fb079366deccd6
gjjsgjj <a670155704g@qq.com>
2021-06-30 17:46:51 +0800

    8-13 在文档页面点击文档节点时，获取并显示文档页面

--- web/src/views/doc.vue
+++ web/src/views/doc.vue
@@ -6,20 +6,21 @@
           <a-tree
               v-if="level1.length>0"
               :tree-data="level1"
+              @select="onSelect"
               :replace-fields="{title: 'name',key: 'id',children: 'children'}"
               :defaultExpandAll="true"
           >
           </a-tree>
         </a-col>
         <a-col :span="18">
-
+          <div :innerHTML="html"></div>
         </a-col>
       </a-row>
     </a-layout-content>
   </a-layout>
 </template>
 
-<script>
+<script lang="ts">
 import {defineComponent, onMounted, ref} from "vue";
 import {message} from "ant-design-vue";
 import axios from "axios";
@@ -27,26 +28,50 @@ import {Tool} from "@/util/tool";
 import {useRoute} from "vue-router";
 export default defineComponent({
   name: 'doc',
-  setup(){
+  setup: function () {
+    const html = ref()
     const level1 = ref();
     level1.value = [];
-    const route=new useRoute()
+    const route = new useRoute()
     const handleQuery = () => {
-      axios.get("/doc/all/"+route.query.ebookId).then((response) => {
+      axios.get("/doc/all/" + route.query.ebookId).then((response) => {
         const data = response.data;
-        if(data.success){
-          level1.value = Tool.array2Tree(data.content,0);
+        if (data.success) {
+          level1.value = Tool.array2Tree(data.content, 0);
           console.log(level1.value);
-        }else{
+        } else {
           message.error(data.message);
         }
       })
     }
-    onMounted(()=>{
+
+    /**
+     * 内容查询
+     **/
+    const handleQueryContent = (id:number) => {
+      axios.get("/doc/find-content/" + id).then((response) => {
+        const data = response.data;
+        if (data.success) {
+          html.value=data.content
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
+
+    const onSelect=(selectedKeys:any,info:any)=>{
+      if (Tool.isNotEmpty(selectedKeys)) {
+        handleQueryContent(selectedKeys[0])
+      }
+    }
+
+    onMounted(() => {
       handleQuery();
     })
-    return{
+    return {
       level1,
+      html,
+      onSelect
     }
   }
 })
