efeb4b510904e591937088c7db7741e2aa971c70
gjjsgjj <a670155704g@qq.com>
2021-07-02 00:51:33 +0800

    9-8 前端增加登录框

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -6,23 +6,101 @@
         mode="horizontal"
         :style="{ lineHeight: '64px' }"
     >
-      <a-menu-item key="/"><router-link to="/" />首页</a-menu-item>
-      <a-menu-item key="/admin/ebook"><router-link to="/admin/ebook" />电子书管理</a-menu-item>
-      <a-menu-item key="/admin/category"><router-link to="/admin/category" />分类管理</a-menu-item>
-      <a-menu-item key="/admin/user"><router-link to="/admin/user" />用户管理</a-menu-item>
-      <a-menu-item key="/about"><router-link to="/about" />关于我们</a-menu-item>
+      <a-menu-item key="/">
+        <router-link to="/">首页</router-link>
+      </a-menu-item>
+      <a-menu-item key="/admin/user">
+        <router-link to="/admin/user">用户管理</router-link>
+      </a-menu-item>
+      <a-menu-item key="/admin/ebook">
+        <router-link to="/admin/ebook">电子书管理</router-link>
+      </a-menu-item>
+      <a-menu-item key="/admin/category">
+        <router-link to="/admin/category">分类管理</router-link>
+      </a-menu-item>
+      <a-menu-item key="/about">
+        <router-link to="/about">关于我们</router-link>
+      </a-menu-item>
+      <a class="login-menu" @click="showLoginModal">
+        <span>登录</span>
+      </a>
     </a-menu>
+
+    <a-modal
+        title="登录"
+        v-model:visible="loginModalVisible"
+        :confirm-loading="loginModalLoading"
+        @ok="login"
+    >
+      <a-form :model="user" :label-col="{ span: 6 }" :wrapper-col="{ span: 18 }">
+        <a-form-item label="登陆名">
+          <a-input v-model:value="user.loginName" />
+        </a-form-item>
+
+        <a-form-item label="密码" >
+          <a-input v-model:value="user.password"/>
+        </a-form-item>
+      </a-form>
+    </a-modal>
   </a-layout-header>
 </template>
 
 
-<script>
-export default {
-  name: "the-header",
+<script lang="ts">
+import {defineComponent, ref} from "vue";
 
-}
+export default defineComponent({
+  name: "the-header",
+  setup(){
+
+    // -------- 表单 ---------
+    const user = ref({
+      loginName:"test",
+      password:"test"
+    });
+    const loginModalVisible = ref(false);
+    const loginModalLoading = ref(false);
+    const showLoginModal=()=>{
+      loginModalVisible.value = true;
+      // user.value = Tool.copy(record)
+    }
+    const login=()=>{
+      console.log("开始登录")
+    }
+
+    // const handleLoginModalOk = () => {
+    //   loginModalLoading.value = true;
+    //
+    //   axios.post("/user/login", user.value).then((response) => {
+    //     loginModalLoading.value = false;
+    //     const data = response.data;
+    //     if (data.success) {
+    //       loginModalVisible.value = false;
+    //     } else {
+    //       message.error(data.message)
+    //     }
+    //   });
+    // };
+
+
+
+
+
+    return{
+      loginModalVisible,
+      loginModalLoading,
+      user,
+      login,
+      showLoginModal
+    }
+  }
+})
 </script>
 
-<style scoped>
-
+<style>
+.login-menu {
+  color: white;
+  margin-left: 800px;
+  order: 999;
+}
 </style>
