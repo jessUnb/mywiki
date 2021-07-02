51ea2ec0b498ed3be6c5015bc138c47ede3c3391
gjjsgjj <a670155704g@qq.com>
2021-07-02 17:50:02 +0800

    9-9 vuex 整合 sessionStorage，解决刷新数据丢失问题

--- web/src/components/the-footer.vue
+++ web/src/components/the-footer.vue
@@ -1,6 +1,6 @@
 <template>
   <a-layout-footer style="text-align: center">
-    MyWiki ，欢迎 {{user.name}}
+    MyWiki <span v-show="user.id">，欢迎 {{user.name}}</span>
   </a-layout-footer>
 </template>
 
