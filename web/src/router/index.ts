c90cfc4b52dae1d30cd2b2963cb162dd38bb1996
gjjsgjj <a670155704g@qq.com>
2021-07-02 22:58:33 +0800

    9-12 增加路由登录拦截，未登录时跳到首页

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -6,17 +6,21 @@ import AdminCategory from '../views/admin/admin-category.vue'
 import AdminDoc from '../views/admin/admin-doc.vue'
 import AdminUser from '../views/admin/admin-user.vue'
 import Doc from '../views/doc.vue'
+import {Tool} from "@/util/tool";
+import store from "@/store";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
     component: Home
+
   },
   {
     path: '/about',
     name: 'About',
     component: About
+
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
@@ -25,22 +29,34 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/admin/ebook',
     name: 'AdminEbook',
-    component: AdminEbook
+    component: AdminEbook,
+    meta:{
+      loginRequire:true
+    }
   },
   {
     path: '/admin/category',
     name: 'AdminCategory',
-    component: AdminCategory
+    component: AdminCategory,
+    meta:{
+      loginRequire:true
+    }
   },
   {
     path: '/admin/doc',
     name: 'AdminDoc',
-    component: AdminDoc
+    component: AdminDoc,
+    meta:{
+      loginRequire:true
+    }
   },
   {
     path: '/admin/user',
     name: 'AdminUser',
-    component: AdminUser
+    component: AdminUser,
+    meta:{
+      loginRequire:true
+    }
   },
   {
     path: '/doc',
@@ -53,5 +69,24 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+
+// 路由登录拦截
+router.beforeEach((to, from, next) => {
+  // 要不要对meta.loginRequire属性做监控拦截
+  if (to.matched.some(function (item) {
+    console.log(item, "是否需要登录校验：", item.meta.loginRequire);
+    return item.meta.loginRequire
+  })) {
+    const loginUser = store.state.user;
+    if (Tool.isEmpty(loginUser)) {
+      console.log("用户未登录！");
+      next('/');
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+});
 
 export default router
