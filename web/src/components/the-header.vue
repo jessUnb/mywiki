0f625801cccb78d142245dc9b51ec9ee55e4c286
gjjsgjj <a670155704g@qq.com>
2021-07-02 22:32:22 +0800

    9-12 前端未登录时，隐藏菜单

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -9,14 +9,14 @@
       <a-menu-item key="/">
         <router-link to="/">首页</router-link>
       </a-menu-item>
-      <a-menu-item key="/admin/user">
+      <a-menu-item key="/admin/user" :style="user.id? {} : {display:'none'}">
         <router-link to="/admin/user">用户管理</router-link>
       </a-menu-item>
-      <a-menu-item key="/admin/ebook">
+      <a-menu-item key="/admin/ebook" :style="user.id? {} : {display:'none'}">
         <router-link to="/admin/ebook">电子书管理</router-link>
       </a-menu-item>
-      <a-menu-item key="/admin/category">
-        <router-link to="/admin/category">分类管理</router-link>
+      <a-menu-item key="/admin/category" :style="user.id? {} : {display:'none'}">
+        <router-link to="/admin/category" >分类管理</router-link>
       </a-menu-item>
       <a-menu-item key="/about">
         <router-link to="/about">关于我们</router-link>
